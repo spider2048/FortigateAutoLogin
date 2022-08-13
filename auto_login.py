4d3433d76b84b69fe026d8cefed3f6702d6e59fc
Quasar <98212676+quasar-nova@users.noreply.github.com>
2022-08-14 00:10:29 +0530

    Update auto_login.py

--- auto_login.py
+++ auto_login.py
@@ -9,5 +9,5 @@ magic = str(soup.findAll('input')[1]).split('\"')[-2]
 with open("\\Path\\To\\Cred_File\\Fortinet.json") as fcreds:
     creds = json.load(fcreds)
     username, password = creds.values()
-
-final_login = subprocess.check_output(["curl", "https://172.25.0.1:1000/", "-d" , f"4Tredir=https%3A%2F%2F172.25.0.1%3A1000%2Flogin%3F&magic={magic}&username={username}&password={password}", "-k"])
+    
+subprocess.check_output(["curl", "https://172.25.0.1:1000/", "-d" , f"4Tredir=https%3A%2F%2F172.25.0.1%3A1000%2Flogin%3F&magic={magic}&username={username}&password={password}", "-k"])
